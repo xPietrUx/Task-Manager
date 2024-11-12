49a5497e4f00635c374533e7b8b8e8149ca3becf
Piotr <piotrdusinski01@gmail.com>
2024-11-12 17:37:31 +0100

    Main configuration Express and initialization all middleware

--- task-manager-backend/src/app.ts
+++ task-manager-backend/src/app.ts
@@ -1,1 +1,16 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app: Application = express();
+
+// Middleware JSON parser
+app.use(express.json());
+
+// Endpoint test
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+export default app;
