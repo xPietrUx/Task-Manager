1f63badf6be5590fe680f5637aa1579043282fc2
Piotr <piotrdusinski01@gmail.com>
2024-11-12 17:54:48 +0100

    Run server and connect to DB

--- task-manager-backend/src/server.ts
+++ task-manager-backend/src/server.ts
@@ -1,1 +1,19 @@
+import app from './app';
+import sequelize from './config/database';
 
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to the PostgreSQL database.');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database: ', error);
+  }
+};
+
+startServer();
