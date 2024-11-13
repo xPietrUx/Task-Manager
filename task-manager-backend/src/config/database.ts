2e094613581151628be36d94fa390dc354296285
Piotr <piotrdusinski01@gmail.com>
2024-11-13 18:42:32 +0100

    Updated database.ts to sequelize-typescript

--- task-manager-backend/src/config/database.ts
+++ task-manager-backend/src/config/database.ts
@@ -1,16 +1,16 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASSWORD as string,
-    {
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        dialect: 'postgres',
-        logging : false
-    }
-);
+dotenv.config();
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME as string,
+  username: process.env.DB_USER as string,
+  password: process.env.DB_PASSWORD as string,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  dialect: 'postgres',
+  logging: false,
+});
 
 export default sequelize;
