59d14126854fd78cd50a34571ad02693c249ccd2
Piotr <piotrdusinski01@gmail.com>
2024-11-16 13:50:51 +0100

    Prepared to test API

--- task-manager-backend/src/server.ts
+++ task-manager-backend/src/server.ts
@@ -1,19 +1,34 @@
-import app from './app';
-import sequelize from './config/database';
+import express from 'express';
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+const port = 3000;
 
-const startServer = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connected to the PostgreSQL database.');
+app.use(express.json());
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error('Unable to connect to the database: ', error);
-  }
-};
+app.get('/', (req, res) => {
+  res.send('Server works properly');
+});
 
-startServer();
+app.get('/tasks', (req, res) => {
+  res.json({ tasks: ['Task 1', 'Task 2', 'Task 3'] });
+});
+
+app.post('/tasks', (req, res) => {
+  const newTask = req.body;
+  res.status(201).json({ message: 'Task created', task: newTask });
+});
+
+app.put('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const updateTask = req.body;
+  res.json({ message: `Task ${id} updated`, task: updateTask });
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ message: `Task ${id} deleted` });
+});
+
+app.listen(port, () => {
+  console.log(`Server runs on http://localhost:${port}`);
+});
